ea1d840d986bab57a751be2cd59a19b67743da92
Alexey S <alexey787@ya.ru>
2022-06-13 01:00:52 +1000

    add two function v3

--- js/main.js
+++ js/main.js
@@ -1,16 +1,57 @@
-function getRandom(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+const COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!',
+];
 
-function isLengthTrue(inputValue, maxLength) {
-  if (inputValue.length <= maxLength) {
-    return true;
-  }
-  return false;
-}
+const NAMES = [
+  'Иван',
+  'Хуан Себастьян',
+  'Мария',
+  'Кристоф',
+  'Виктор',
+  'Юлия',
+  'Люпита',
+  'Вашингтон',
+];
 
-getRandom(50, 150);
+const DESCRIPTION = [
+  'Фото природы',
+  'Фото прибоя',
+  'Фото гор',
+  'Фото заката',
+  'Фото рассвета',
+  'Фото замка',
+  'Фото пустыни',
+  'Фото водопада',
+];
 
-isLengthTrue('абвгдежзиклмнопр', 10);
+const getRandomPositiveInteger = (a, b) => {
+  const lower = Math.ceil(Math.min(Math.abs(a), Math.abs(b)));
+  const upper = Math.floor(Math.max(Math.abs(a), Math.abs(b)));
+  const result = Math.random() * (upper - lower + 1) + lower;
+  return Math.floor(result);
+};
+
+const getRandomArrayElement = (elements) => elements[getRandomPositiveInteger(0, elements.length - 1)];
+
+const createFoto = () => ({
+  id: getRandomPositiveInteger(1, 25),
+  url: `photos/${  getRandomPositiveInteger(1, 25) }.jpg`,
+  description: getRandomArrayElement(DESCRIPTION),
+  likes: getRandomPositiveInteger(15, 200),
+  comments: {
+    id: getRandomPositiveInteger(1, 25),
+    avatar: `img/avatar-${  getRandomPositiveInteger(1, 6)  }.svg`,
+    message: getRandomArrayElement(COMMENTS),
+    name: getRandomArrayElement(NAMES),
+  },
+});
+
+const similarFotos = Array.from({length: 25}, createFoto);
+similarFotos();
 
 
