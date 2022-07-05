3dd5a789d164398960e095ba59836d685bedb38b
Alexey S <alexey787@ya.ru>
2022-07-06 06:10:51 +1000

    do picture effect

--- js/form.js
+++ js/form.js
@@ -1,4 +1,4 @@
-import {isEscapeKey} from './util.js';
+import { isEscapeKey } from './util.js';
 
 const userModalElement = document.querySelector('.img-upload__overlay');
 const userModalCloseElement = document.querySelector('.img-upload__cancel');
@@ -6,6 +6,37 @@ const userUploadFile = document.querySelector('#upload-file');
 const form = document.querySelector('.img-upload__form');
 const inputs = form.querySelectorAll('input, textarea');
 const sendBtn = form.querySelector('.img-upload__submit');
+const smallerBtnNode = form.querySelector('.scale__control--smaller');
+const biggerBtnNode = form.querySelector('.scale__control--bigger');
+const inputScaleNode = form.querySelector('.scale__control--value');
+const re = /[0-9]+/;
+const imgNode = form.querySelector('.img-upload__preview');
+const effectCollection = form.querySelector('.effects__list');
+form.querySelector('.img-upload__effect-level').style.display = 'none';
+
+effectCollection.onclick = function (evt) {
+  if (evt.target.nodeName === 'SPAN') {
+    imgNode.classList.forEach((item) => {
+      if(item !== 'img-upload__preview') {imgNode.classList.remove(item);
+      }
+    });
+    imgNode.classList.add(evt.target.classList[1]);
+  }
+};
+
+smallerBtnNode.onclick = function () {
+  let val = Number(inputScaleNode.value.match(re));
+  val = (val - 25 === 0) ? 25 : val - 25;
+  inputScaleNode.value = `${val  }%`;
+  imgNode.style.transform = `scale(${val/100})`;
+};
+
+biggerBtnNode.onclick = function () {
+  let val = Number(inputScaleNode.value.match(re));
+  val = (val + 25 > 100) ? 100 : val + 25;
+  inputScaleNode.value = `${val  }%`;
+  imgNode.style.transform = `scale(${val/100})`;
+};
 
 
 const pristine = new Pristine(form, {
@@ -15,8 +46,6 @@ const pristine = new Pristine(form, {
 
 
 form.addEventListener('keypress', () => {
-  //evt.preventDefault();
-
   const isValid = pristine.validate();
   if (isValid) {
     sendBtn.disabled = false;
@@ -34,18 +63,18 @@ const clearInputValue = () => {
 const onPopupEscKeydown = (evt) => {
   if (isEscapeKey(evt)) {
     evt.preventDefault();
-    closeEditImageForm ();
+    closeEditImageForm();
   }
 };
 
-function openEditImageForm () {
+function openEditImageForm() {
   sendBtn.disabled = true;
   userModalElement.classList.remove('hidden');
   document.body.classList.add('modal-open');
   document.addEventListener('keydown', onPopupEscKeydown);
 }
 
-function closeEditImageForm () {
+function closeEditImageForm() {
   userModalElement.classList.add('hidden');
   document.body.classList.remove('modal-open');
   clearInputValue();
@@ -53,11 +82,11 @@ function closeEditImageForm () {
 }
 
 const onFileSelect = () => {
-  openEditImageForm ();
+  openEditImageForm();
 };
 
 userModalCloseElement.onclick = () => {
-  closeEditImageForm ();
+  closeEditImageForm();
 };
 
 userUploadFile.addEventListener('change', onFileSelect);
